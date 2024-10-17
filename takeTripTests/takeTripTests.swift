8bbd26fbe26619a98d96588b1215c145ca11bbfc
89Explorer <leral3@naver.com>
2024-10-18 02:30:09 +0900

    Initial Commit

---
+++ takeTripTests/takeTripTests.swift
@@ -0,0 +1,17 @@
+//
+//  takeTripTests.swift
+//  takeTripTests
+//
+//  Created by 권정근 on 10/18/24.
+//
+
+import Testing
+@testable import takeTrip
+
+struct takeTripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
