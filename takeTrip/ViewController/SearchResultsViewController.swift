be300587c156202f77a5c88113ba35ccb0c3702f
89Explorer <leral3@naver.com>
2024-10-25 15:46:00 +0900

    feat: 서치바를 사용해여 검색 설정 - 서치바를 이용해서 검색을 진행할 예정

---
+++ takeTrip/ViewController/SearchResultsViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SearchResultsViewController.swift
+//  takeTrip
+//
+//  Created by 권정근 on 10/25/24.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    
+    // MARK: - Variables
+    /// 결과를 받아오는 변수 배열
+    var spotResults: [String] = []
+
+    // MARK: - UI Components
+    let spotTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    
+    // MARK: - Initializations
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+        configureConstraints()
+        configureTableView()
+    }
+    
+    // MARK: - Layouts
+    private func configureConstraints() {
+        view.addSubview(spotTableView)
+        
+        let spotTableViewConstraints = [
+            spotTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            spotTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            spotTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            spotTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(spotTableViewConstraints)
+    }
+    
+    // MARK: - Functions
+    func configureTableView() {
+        spotTableView.delegate = self
+        spotTableView.dataSource = self
+        spotTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func updateTableView(with results: [String]) {
+        self.spotResults = results
+        spotTableView.reloadData()
+    }
+}
+
+
+extension SearchResultsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spotResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = spotResults[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 셀 선택 시 동작할 코드를 여기에 작성
+        print("선택된 항목: \(spotResults[indexPath.row])")
+    }
+}
--- takeTrip/ViewController/SearchViewController.swift
+++ takeTrip/ViewController/SearchViewController.swift
@@ -8,22 +8,79 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    var spotResults: [AttractionItem] = []
+    
+    // MARK: - UI Components
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "어디 가실지 장소를 검색하세요"
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }()
+    
+    // MARK: - Initializations
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        searchBar.delegate = self
+        configureConstraints()
+        configureNavigationItem()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Layouts
+    private func configureConstraints() {
+        view.addSubview(searchBar)
+        
+        let searchBarConstraints = [
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(searchBarConstraints)
     }
-    */
+    
+    
+    // MARK: - Functions
+    /// 네비게이션 아이템 설정 함수
+    func configureNavigationItem() {
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "Search"
+        titleLabel.textColor = .label
+        // titleLabel.font = UIFont(name: "YeongdoOTF-Bold", size: 28)
+        titleLabel.font = .systemFont(ofSize: 28, weight: .black)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+    }
+}
 
+// MARK: - UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+    
+    // 사용자가 서치 버트을 눌렀을 때 호출되는 함수
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let query = searchBar.text, !query.isEmpty else {
+            print("검색어가 비어있습니다.")
+            return
+        }
+        
+        // 입력된 검색어를 콘솔에 출력
+        //print("사용자가 입력한 검색어: \(query)")
+        
+        // 검색어에 맞는 결과를 찾아서 SearchResultsViewController로 전달
+        let filteredResults = ["어린이 공원", "어린이 박물관", "어린이 도서관", "키즈카페 어린이"]
+        
+        // SearchResultsViewController 인스턴스 생성
+        let resultsViewController = SearchResultsViewController()
+        resultsViewController.updateTableView(with: filteredResults)
+        
+        navigationController?.pushViewController(resultsViewController, animated: true)
+        // 외부 API 호출을 이 부분에서 처러
+    }
 }
